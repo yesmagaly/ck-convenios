63ad86961805ac77b300253bd13c13e88f7170f9
Joseph Ramos <joseph.ramos@taller.net.br>
2015-08-28 23:05:55 -0500

    Set right router to serve static pdf file.

--- app/extensions/agreement/agreement.js
+++ app/extensions/agreement/agreement.js
@@ -28,26 +28,30 @@ agreement.find = function(type, query, callback) {
  * The route() hook.
  */
 agreement.route = function(routes, callback) {
-
   var newRoutes = {};
-
 
   newRoutes['/pdf-report'] = {
     access: true,
     callback: function(request, response, callback) {
       var doc = new PDFDocument();
 
-      doc.pipe(fs.createWriteStream('out.pdf'));
-
       doc.text('Hola', 50, 70);
 
-      response.writeHead(200, {"Content-Type": "application/pdf"});
+      response.writeHead(200, {
+        'Content-Type': 'application/pdf',
+        // 'Access-Control-Allow-Origin': '*',
+        // @NOTE: this configuration if whant dowload file by default.
+        // 'Content-Disposition': 'attachment; inline; filename=out.pdf'
+      });
+
+      // @TODO: move this file to correct directory.
+      doc.pipe(fs.createWriteStream('out.pdf'));
 
       doc.pipe(response);
       doc.end();
-    }
+    },
+    router: 'static'
   };
-
 
   callback(null, newRoutes);
 };
