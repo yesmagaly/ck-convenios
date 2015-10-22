378b239cae47ed5aebaadcfe9d5bdee99fd02809
Joseph Ramos <joseph.ramos@taller.net.br>
2015-10-22 01:12:19 -0500

    Changed port application to 3020

--- tests/bdd/spec.js
+++ tests/bdd/spec.js
@@ -1,10 +1,11 @@
 describe('OCNI application', function () {
-  
-  it('Load the site', function () {
+
+  it('should load the site', function () {
+
     // Load the site.
-    browser.get('http://localhost:3000');
+    browser.get('http://localhost:3020');
     browser.driver.sleep(3000);
-    
+
     var title = element(by.css('.navbar-brand .ng-scope'));
 
     expect(title.getText()).toContain('Convenios Ágiles');
