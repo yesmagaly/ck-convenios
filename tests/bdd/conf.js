8d83c6d5502ce653679d1fa707ebcdc3d50d6652
yesmagaly <yesmagaly@gmail.com>
2015-10-22 11:54:54 -0500

    merged oc-004-12 to this branch

--- tests/bdd/conf.js
+++ tests/bdd/conf.js
@@ -1,8 +1,23 @@
 module.exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
+
   specs: [
     'spec.js',
     'agreement/**.spec.js',
     // 'institution/**.spec.js',
-  ]
+  ],
+
+  framework: 'jasmine2',
+
+  onPrepare: function() {
+    var SpecReporter = require('jasmine-spec-reporter');
+    // add jasmine spec reporter
+    jasmine.getEnv().addReporter(new SpecReporter({
+      displayStacktrace: 'all',
+      displayFailuresSummary: true,
+      displayFailedSpec: true,
+      displaySuiteNumber: true,
+      displaySpecDuration: true
+    }));
+  }
 };
