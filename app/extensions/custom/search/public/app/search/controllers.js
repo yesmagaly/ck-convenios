e1d76a64a8b0c21898431eaa9d19d9b54d666d78
yesmagaly <yesmagaly@gmail.com>
2016-01-12 09:29:21 -0500

    Updated search form to use only one field (Title), extended controller to search list panel and added controller to update the search list panel.

--- app/extensions/custom/search/public/app/search/controllers.js
+++ app/extensions/custom/search/public/app/search/controllers.js
@@ -10,7 +10,31 @@
    */
   .controller('SearchFormCtrl', [
     '$scope',
-    function ($scope) {
-      console.log('SearchFormCtrl');
+    '$rootScope',
+    function ($scope, $rootScope) {
+      $scope.$watch('viewForm', function() {
+
+        if ($scope.viewForm) {
+          $scope.viewForm.then(function(response) {
+            $rootScope.$broadcast('searchAgreementstUpdated', response);
+          });
+        }
+
+      });
+
+    }
+  ])
+
+  .controller('SearchListCtrl', [
+    '$scope',
+    '$rootScope',
+    function ($scope, $rootScope) {
+      $scope.$on('searchAgreementstUpdated', function (event, result) {
+        console.log(result);
+
+        $rootScope.$broadcast('agreementList', function($scope) {
+          $scope.items = result;
+        })
+      });
     }
   ])
